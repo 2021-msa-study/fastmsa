6e56b3bc1d9a49d424a168d9c767dfe38a9a3d49
Joseph Kim <cloudeyes@gmail.com>
2021-04-09 08:34:14 +0900

    Fix docker scripts to auto-restart

--- scripts/run_postgres.sh
+++ scripts/run_postgres.sh
@@ -1,2 +1,2 @@
-docker run --name pgdev -d -p 5432:5432 -e POSTGRES_PASSWORD=test postgres:9.6-alpine
+docker run --name pgdev -d --restart unless-stopped -p 5432:5432 -e POSTGRES_PASSWORD=test postgres:9.6-alpine
 
