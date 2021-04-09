faa09d9ee7eb746c20c4d9665cd88afe9e7dfb61
Joseph Kim <cloudeyes@gmail.com>
2021-04-09 21:05:51 +0900

    Add package setup scripts

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+from os import path
+
+this_dir = path.abspath(path.dirname(__file__))
+with open(path.join(this_dir, "README.md")) as f:
+    long_description = f.read()
+
+setup(
+    name="fastmsa",
+    packages=["fastmsa"],
+    version="0.1",
+    license="MIT",
+    description="FastMSA - full-stack framework for microservice architecture applications",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Joseph Kim, Benzamin Yoon",
+    author_email="cloudeyes@gmail.com",
+    url="https://github.com/2021-msa-study/fastmsa",
+    download_url="https://github.com/2021-msa-study/fastmsa/archive/v0.1.tar.gz",
+    keywords=["fastmsa", "microservice" "framework", "sqlalchemy", "fastapi"],
+    install_requires=[
+        "fastapi",
+        "flask",
+        "sqlalchemy",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
