ff5daf6d005e6ea4b47fbc8a779a260ed5a3b6eb
Yongki Kim <cloudeyes@gmail.com>
2021-04-24 12:07:00 +0900

    Reformat templates

--- fastmsa/templates/app/app/routes/sample.py
+++ fastmsa/templates/app/app/routes/sample.py
@@ -1,9 +1,10 @@
 from fastmsa.api import get
-
 from fastmsa.repo import SqlAlchemyRepository
-from {{msa.name}}.domain.models import User
 
-@get('/users')
+from ..domain.models import User
+
+
+@get("/users")
 def read_users():
     with SqlAlchemyRepository(User) as users:
         return users.all()
