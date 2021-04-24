0db8a1a35377880df2c98a8d7885900a1f9d30e1
Yongki Kim <cloudeyes@gmail.com>
2021-04-24 17:28:59 +0900

    Fix schema

--- fastmsa/schema.py
+++ fastmsa/schema.py
@@ -1,6 +1,5 @@
 """스키마 변환, 검증 조작등의 기능을 담당하는 모듈입니다."""
-from typing import Any, Type, cast, Callable, Optional
-from dataclasses import is_dataclass
+from typing import Any, Type, cast, Callable, Optional, TypeVar, Union
 
 from pydantic import BaseModel
 from pydantic.main import ModelMetaclass
@@ -8,39 +7,40 @@ from pydantic.main import ModelMetaclass
 
 AnyType = Type[Any]
 
+D = TypeVar("D")
+T = TypeVar("T")
+
 
 def from_dataclass(
-    DataClass: AnyType, excludes: Optional[list[str]] = None
-) -> Callable[[AnyType], Type[Any]]:
+    DataClass: Type[D], excludes: Optional[list[str]] = None
+) -> Callable[[Type[T]], Type[Union[BaseModel, D, T]]]:
     """`dataclasses.dataclass` 모델을 Pydantic `BaseModel` 로 변환합니다."""
 
-    def _wrapper(TargetClass: AnyType):
-        def _get_model(DataClass: AnyType) -> Type[Any]:
-            annotations = list(
+    def _wrapper(TargetClass: Type[T]):
+        def _get_model(DataClass: Type[D], excludes=[]) -> AnyType:
+            from dataclasses import is_dataclass
+
+            annotations = dict(
                 (name, type)
                 for name, type in DataClass.__annotations__.items()
-                if excludes and name not in excludes
+                if not excludes or name not in excludes
             )
-            for field_name, field_type in annotations:
-                if excludes and field_name in excludes:
-                    continue
 
+            for field_name, field_type in annotations.items():
                 if is_dataclass(field_type):
                     field = _get_model(field_type)
-                    DataClass.__annotations__[field_name] = field
+                    annotations[field_name] = field
 
             namespace = {
-                "__annotations__": dict(annotations),
+                "__annotations__": annotations,
                 "__module__": TargetClass.__module__,
                 "__qualname__": TargetClass.__qualname__,
             }
             return cast(
-                Type[Any],
-                ModelMetaclass(
-                    TargetClass.__name__, (BaseModel, TargetClass), namespace
-                ),
+                AnyType,
+                ModelMetaclass(TargetClass.__name__, (BaseModel,), namespace),
             )
 
-        return _get_model(DataClass)
+        return _get_model(DataClass, excludes=excludes)
 
     return _wrapper
--- fastmsa/templates/app/app/routes/sample.py
+++ fastmsa/templates/app/app/routes/sample.py
@@ -1,10 +1,22 @@
-from fastmsa.api import get
+from fastmsa.api import get, post
 from fastmsa.repo import SqlAlchemyRepository
+from fastmsa.schema import from_dataclass
 
 from ..domain.models import User
 
 
-@get("/users")
+@from_dataclass(User, excludes=["email"])
+class UserSchema:
+    ...
+
+
+@post("/users/")
+def write_users(user: UserSchema):
+    with SqlAlchemyRepository(User) as users:
+        users.add(data)
+
+
+@get("/users", response_model=UserSchema)
 def read_users():
     with SqlAlchemyRepository(User) as users:
         return users.all()
