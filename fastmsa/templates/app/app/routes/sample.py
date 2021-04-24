696ca67a32ee577e105040554bfe18d92d644f1e
Yongki Kim <cloudeyes@gmail.com>
2021-04-24 17:40:20 +0900

    Fix sample

--- fastmsa/templates/app/app/routes/sample.py
+++ fastmsa/templates/app/app/routes/sample.py
@@ -1,22 +1,31 @@
+"""샘플 FastAPI 엔드포인트."""
+from typing import cast
 from fastmsa.api import get, post
 from fastmsa.repo import SqlAlchemyRepository
-from fastmsa.schema import from_dataclass
+from fastmsa.schema import from_dataclass, BaseModel
 
 from ..domain.models import User
 
 
 @from_dataclass(User, excludes=["email"])
-class UserSchema:
+class UserReadSchema:  # noqa
     ...
 
 
-@post("/users/")
-def write_users(user: UserSchema):
+@from_dataclass(User)
+class UserCreateSchema:  # noqa
+    ...
+
+
+@post("/users")
+def create_user(user: UserCreateSchema):
+    new_user = User(**cast(BaseModel, user).dict())
     with SqlAlchemyRepository(User) as users:
-        users.add(data)
+        users.add(new_user)
+        users.session.commit()
 
 
-@get("/users", response_model=UserSchema)
+@get("/users", response_model=list[UserReadSchema])
 def read_users():
     with SqlAlchemyRepository(User) as users:
         return users.all()
