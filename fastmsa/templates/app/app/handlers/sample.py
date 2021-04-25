50b718248e5a7831707e6aa8d58c55fb905fd0af
Joseph Kim <cloudeyes@gmail.com>
2021-04-26 00:11:13 +0900

    Update sample

--- fastmsa/templates/app/app/handlers/sample.py
+++ fastmsa/templates/app/app/handlers/sample.py
@@ -1,3 +1,4 @@
+"""샘플 이벤트 핸들러."""
 from fastmsa.event import on_event
 
 from ..domain.events import SampleEvent
@@ -5,5 +6,5 @@ from ..domain.events import SampleEvent
 
 @on_event(SampleEvent)
 def print_sample(event: SampleEvent):
-    """샘플 이벤트 핸들러."""
+    """샘플 이벤트 메세지를 출력하는 핸들러."""
     print(event.msg)
