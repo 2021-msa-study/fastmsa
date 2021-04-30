e3b84a7ebaa4529a9879d80adbf389319565437b
Joseph Kim <cloudeyes@gmail.com>
2021-05-01 01:33:07 +0900

    Fix typos

--- fastmsa/event.py
+++ fastmsa/event.py
@@ -110,8 +110,8 @@ class MessageBus(AbstractMessageHandler):
     ):
         """핸들러의 파라메터를 보고 적절한 의존성을 주입하여 핸들러를 호출합니다.
 
-        예를 들어 `def a_handler(uow, msgtbroker)` 와 같은 핸들러가 있을 경우 `uow` 나
-        `broker` 같은 이름은 외부 의존성을 가리킵니다.
+        예를 들어 `def a_handler(uow, broker)` 와 같은 핸들러가 있을 경우 `uow` 나
+        `broker`(외부 메세지 브로커) 같은 이름은 외부 의존성을 가리킵니다.
         """
         params = self.params_cache.get(handler)
         if not params:
